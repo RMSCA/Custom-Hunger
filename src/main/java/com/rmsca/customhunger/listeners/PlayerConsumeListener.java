3ff1b6d15302e97481a0132db3bf45eca0d4e2c7
RMSCA <howell.rmsca@outlook.com>
2022-04-28 15:30:46 -0700

    Bug fix

--- src/main/java/com/rmsca/customhunger/listeners/PlayerConsumeListener.java
+++ src/main/java/com/rmsca/customhunger/listeners/PlayerConsumeListener.java
@@ -4,17 +4,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
-public class PlayerConsumeListener implements Listener {
-    private int bread = getConfig().getInt("Bread");
+import org.bukkit.plugin.java.JavaPlugin;
+public class PlayerConsumeListener extends JavaPlugin implements Listener {
+    private final int BREAD = getConfig().getInt("Bread");
     @EventHandler
     public void onPlayerConsume(PlayerItemConsumeEvent e) {
         Player p = e.getPlayer();
         if(e.getItem().getType() == Material.BREAD) {
             System.out.println("Event Triggered!");
-            if(p.getFoodLevel() > (20 - bread)) {
+            if(p.getFoodLevel() > (20 - BREAD)) {
                 p.setFoodLevel(20);
             } else {
-                p.setFoodLevel(p.getFoodLevel() + bread);
+                p.setFoodLevel(p.getFoodLevel() + BREAD);
             }
             System.out.println("Process complete!");
         }
