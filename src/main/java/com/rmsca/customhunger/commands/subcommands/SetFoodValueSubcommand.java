7d527d969354fd284c65bc6d3ac349f9a7c14d0b
RMSCA <howell.rmsca@outlook.com>
2022-05-02 18:59:54 -0700

    Improvements

--- src/main/java/com/rmsca/customhunger/commands/subcommands/SetFoodValueSubcommand.java
+++ src/main/java/com/rmsca/customhunger/commands/subcommands/SetFoodValueSubcommand.java
@@ -1,15 +1,11 @@
 package com.rmsca.customhunger.commands.subcommands;
 
-import com.rmsca.customhunger.CustomHunger;
 import com.rmsca.customhunger.commands.CommandHelper;
 import com.rmsca.customhunger.commands.Subcommand;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 public class SetFoodValueSubcommand extends Subcommand {
-
-    Plugin plugin = CustomHunger.getPlugin(CustomHunger.class);
 
     @Override
     protected String getName() {
@@ -20,10 +16,12 @@ public class SetFoodValueSubcommand extends Subcommand {
     protected void execute(Player p, String[] args) {
         if (CommandHelper.isInteger(args[1])) {
             p.setFoodLevel(Integer.parseInt(args[1]));
+            p.sendMessage("Your food level is now set to " + Integer.parseInt(args[1]));
         } else {
             try {
                 Player targetPlayer = Bukkit.getPlayerExact(args[1]);
                 targetPlayer.setFoodLevel(Integer.parseInt(args[2]));
+                p.sendMessage(targetPlayer.getDisplayName() + "'s food level is now set to " + Integer.parseInt(args[2]));
             } catch (Exception e) {
                 p.sendMessage("Could not find this player! Check if there is a typo!");
             }
@@ -32,14 +30,14 @@ public class SetFoodValueSubcommand extends Subcommand {
 
     @Override
     protected void execute(String[] args) {
-        try {
-            Player targetPlayer = Bukkit.getPlayerExact(args[1]);
-            targetPlayer.setFoodLevel(Integer.parseInt(args[2]));
-            Bukkit.getConsoleSender().sendMessage(targetPlayer.getDisplayName() + "'s food level is now set to " + Integer.parseInt(args[2]));
-        } catch (Exception e) {
-            if (CommandHelper.isInteger(args[1])) {
-                Bukkit.getConsoleSender().sendMessage("You must specify a player name!");
-            } else {
+        if (CommandHelper.isInteger(args[1])) {
+            Bukkit.getConsoleSender().sendMessage("You must specify a player name!");
+        } else {
+            try {
+                Player targetPlayer = Bukkit.getPlayerExact(args[1]);
+                targetPlayer.setFoodLevel(Integer.parseInt(args[2]));
+                Bukkit.getConsoleSender().sendMessage(targetPlayer.getDisplayName() + "'s food level is now set to " + Integer.parseInt(args[2]));
+            } catch (Exception e) {
                 Bukkit.getConsoleSender().sendMessage("Could not find this player! Check if there is a typo!");
             }
         }
