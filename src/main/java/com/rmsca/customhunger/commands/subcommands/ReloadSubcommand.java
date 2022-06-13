9aca8353b1032770780ae8edadbd33249c754474
RMSCA <howell.rmsca@outlook.com>
2022-06-12 22:16:18 -0700

    Reload command

---
+++ src/main/java/com/rmsca/customhunger/commands/subcommands/ReloadSubcommand.java
@@ -0,0 +1,34 @@
+package com.rmsca.customhunger.commands.subcommands;
+
+import com.rmsca.customhunger.CustomHunger;
+import com.rmsca.customhunger.commands.Subcommand;
+import com.rmsca.customhunger.utils.ChHelper;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class ReloadSubcommand extends Subcommand {
+    Plugin plugin = CustomHunger.getPlugin(CustomHunger.class);
+    @Override
+    public String getName() {
+        return "reload";
+    }
+
+    @Override
+    public String getUsage() {
+        return "Usage: /ch reload";
+    }
+
+    @Override
+    public void execute(Player p, String[] args) {
+        plugin.reloadConfig();
+        if(isReloaded()) {
+            ChHelper.sendMessage(p, "Config reloaded!");
+        } else {
+            ChHelper.sendMessage(p, "Config has not reloaded!");
+        }
+    }
+
+    private boolean isReloaded() {
+        return false;
+    }
+}
